311f108359a4049736345300413c6abe54418da5
eden <1denchen.work@gmail.com>
2023-08-27 22:06:45 +0800

    [feat] 新增會員頁面modal

---
+++ assets/js/user.js
@@ -0,0 +1,31 @@
+//變更密碼
+const swiper = new Swiper('.swiper-container', {
+    initialSlide: 0, // 初始顯示的頁面
+    navigation: {
+        nextEl: '#next-btn', // 下一頁按鈕
+        prevEl: '.swiper-prev-btn'  // 上一頁按鈕
+    },
+    allowTouchMove: false, // 禁止手指滑動
+    spaceBetween: 20,
+});
+
+document.getElementById('next-btn').addEventListener('click', function () {
+    swiper.slideNext();
+});
+
+document.getElementById('prev-btn').addEventListener('click', function () {
+    swiper.slidePrev();
+});
+
+
+// 修改成功彈窗
+const successMessage = document.querySelector('#success-message')
+successMessage.addEventListener('click', function () {
+    Swal.fire({
+        title: "修改成功",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500
+    });
+    $('#pic-modal').modal('hide');
+})
