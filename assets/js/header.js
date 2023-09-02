fed3f8ce72367ab87e1a5febf9343887a9abb90a
panduola666 <panduola666@gmail.com>
2023-09-02 17:22:13 +0800

    [feat] 登入API 簡單串接, 後續處理還須負責人員製作

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,12 @@
+import { User } from '../api';
+
 // 登入註冊modal
 document.addEventListener("DOMContentLoaded", function () {
     const forget = document.querySelector("#forget-btn");
     const back = document.querySelector("#back-btn");
+
+    const loginForm = document.querySelector('.login-form')
+
 
     forget.addEventListener("click", function (e) {
         e.preventDefault();
@@ -11,4 +16,27 @@ document.addEventListener("DOMContentLoaded", function () {
         e.preventDefault();
         document.body.classList.toggle("switch");
     });
+
+
+    // 登入
+    loginForm.addEventListener('submit',async (e) => {
+        e.preventDefault()
+        // 還缺少表單驗證, 目前先串好登入資料供後續人使用
+        const data = {
+            email: loginForm['login-email'].value,
+            password: loginForm['login-password'].value
+        }
+       User.login(data)
+       .then(res => {
+        // 登入成功後續處理
+        console.log(res);
+       })
+       .catch(err => {
+        // 登入失敗後續處理
+        // 情況例如: 帳號密碼不正確
+        console.log(err);
+       })
+       
+    })
 });
+
