7f596d45f500598869ab57fbe1f7ed1e247f79d7
eden <1denchen.work@gmail.com>
2023-09-03 20:04:03 +0800

    [fix] 課程清單RWD + [feat] favicon

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,15 +1,15 @@
-//變更密碼
-const swiper = new Swiper('.swiper-container', {
+//跳轉至變更密碼
+const pwdSwiper = new Swiper('.swiper-container', {
     initialSlide: 0, // 初始顯示的頁面
     navigation: {
-        nextEl: '#next-btn', // 下一頁按鈕
+        nextEl: '#changePwd-btn', // 下一頁按鈕
         prevEl: '.swiper-prev-btn'  // 上一頁按鈕
     },
     allowTouchMove: false, // 禁止手指滑動
     spaceBetween: 20,
 });
 
-document.getElementById('next-btn').addEventListener('click', function () {
+document.getElementById('changePwd-btn').addEventListener('click', function () {
     swiper.slideNext();
 });
 
@@ -18,7 +18,7 @@ document.getElementById('prev-btn').addEventListener('click', function () {
 });
 
 
-// 修改成功彈窗
+//頭像修改成功彈窗
 const successMessage = document.querySelector('#success-message')
 successMessage.addEventListener('click', function () {
     Swal.fire({
