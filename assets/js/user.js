6e940b35b649c79a0e62e3c8f1b917fad4c16a3b
panduola666 <panduola666@gmail.com>
2023-09-03 21:18:17 +0800

    [fix] merge 錯誤樣式

--- assets/js/user.js
+++ assets/js/user.js
@@ -3,14 +3,14 @@ import Swiper from "swiper";
 const swiper = new Swiper('.swiper-container', {
     initialSlide: 0, // 初始顯示的頁面
     navigation: {
-        nextEl: '#next-btn', // 下一頁按鈕
+        nextEl: '#changePwd-btn', // 下一頁按鈕
         prevEl: '.swiper-prev-btn'  // 上一頁按鈕
     },
     allowTouchMove: false, // 禁止手指滑動
     spaceBetween: 20,
 });
 
-document.getElementById('next-btn').addEventListener('click', function () {
+document.getElementById('changePwd-btn').addEventListener('click', function () {
     swiper.slideNext();
 });
 
