52461184a193317020581d368ec03c360b6a9ef8
panduola666 <panduola666@gmail.com>
2023-09-03 20:33:12 +0800

    [fix] 用戶中心 swiper 報錯

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,15 +1,16 @@
+import Swiper from "swiper";
 //跳轉至變更密碼
-const pwdSwiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper-container', {
     initialSlide: 0, // 初始顯示的頁面
     navigation: {
-        nextEl: '#changePwd-btn', // 下一頁按鈕
+        nextEl: '#next-btn', // 下一頁按鈕
         prevEl: '.swiper-prev-btn'  // 上一頁按鈕
     },
     allowTouchMove: false, // 禁止手指滑動
     spaceBetween: 20,
 });
 
-document.getElementById('changePwd-btn').addEventListener('click', function () {
+document.getElementById('next-btn').addEventListener('click', function () {
     swiper.slideNext();
 });
 
