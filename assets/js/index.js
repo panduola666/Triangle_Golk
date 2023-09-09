966357ac42fbc009914d12e81c982425abfa1f91
panduola666 <panduola666@gmail.com>
2023-09-09 17:56:19 +0800

    [feat] swiper 設定

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,29 @@
-// import { User } from '../api/index.js'
-
-// const btn = document.querySelector('.myBtn'); // 獲取 登入 按鈕DOM
-// const apiBtn = document.querySelector('.myBtn2'); // 獲取 其他api測試 按鈕DOM
-// const signOut = document.querySelector('.signOut'); // 獲取 登出 按鈕DOM
-// btn.addEventListener('click', async () => {
-//   const data = {
-//     email: 'admin@gmail.com',
-//     password: '111111',
-//   };
-//     const res =  await User.login(data);
-//     console.log(res);
-// });
-
-// apiBtn.addEventListener('click',async () => {
-//  const res = await User.getNotions(1)
-//  console.log(res);
-// })
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
 
 
-//   signOut.addEventListener('click', async () => {
-//     const res =  await User.signOut();
-//     console.log(res);
-// });
+
+// swiper 配置
+const swiper = new Swiper('.other-courses', {
+  // 配置你使用的 swiper class 名稱
+  slidesPerView: 'auto', // 一頁幾張
+  spaceBetween: 16, // 每一頁的間距
+  centeredSlides: false,
+  loop: true, // 循環播放
+  modules: [Navigation, Autoplay, Pagination],
+  autoplay: {
+    // 自動撥放配置
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-next', // 控制上一頁箭頭用哪個 class
+    prevEl: '.swiper-pre',
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable :true,
+  },
+  });
   
