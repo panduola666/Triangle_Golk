7504ecb95d8573d16158e6e1c7d4b9e63ae28dab
panduola666 <panduola666@gmail.com>
2023-09-13 00:33:31 +0800

    [feat] 額外抽出申請新課 modal

--- assets/js/apply_course.js
+++ assets/js/apply_course.js
@@ -1,65 +1,63 @@
 // 專門用來寫新課申請 API
 import 'https://unpkg.com/@wangeditor/editor@latest/dist/index.js'
-
 import Swal from 'sweetalert2';
+import { Modal } from 'bootstrap';
 
-$('.apply-form').each(function (index) {
-    $(this).on('submit', function (e) {
-        e.preventDefault();
-        $(this).addClass('was-validated');
-    });
-});
+const applyForm = document.querySelector('.apply-form')
+const applyBtn = document.querySelector('.apply-btn')
+const myModal = new Modal(document.getElementById('apply-modal'))
 
-document.getElementById('apply-modal').addEventListener('submit', function (event) {
+applyBtn.addEventListener('click', () =>  myModal.show())
+applyForm.addEventListener('submit', function (event) {
     event.preventDefault(); // 阻止表單的默認提交行為
 
     // 關閉 modal
-    $('#apply-modal').modal('hide');
+    myModal.hide()
 
     // 彈出 SweetAlert 視窗
-    Swal.fire({
-        scrollbarPadding: false,
-        title: "已送出",
-        icon: "success",
-        text: "感謝您的推薦，我們將於 7-14 天內給予您答覆！",
-        showConfirmButton: false,
-        timer: 1500
-    });
+    // Swal.fire({
+    //     scrollbarPadding: false,
+    //     title: "已送出",
+    //     icon: "success",
+    //     text: "感謝您的推薦，我們將於 7-14 天內給予您答覆！",
+    //     showConfirmButton: false,
+    //     timer: 1500
+    // });
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('.apply-form');
-    const inputFields = form.querySelectorAll('input, textarea');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const form = document.querySelector('.apply-form');
+//     const inputFields = form.querySelectorAll('input, textarea');
 
-    // 當表單提交時
-    form.addEventListener('submit', function (event) {
-        event.preventDefault(); // 阻止表單默認提交
+//     // 當表單提交時
+//     form.addEventListener('submit', function (event) {
+//         event.preventDefault(); // 阻止表單默認提交
 
-        // 處理表單提交邏輯...
+//         // 處理表單提交邏輯...
 
-        // 清空輸入框的值
-        inputFields.forEach(function (input) {
-            input.value = '';
-        });
+//         // 清空輸入框的值
+//         inputFields.forEach(function (input) {
+//             input.value = '';
+//         });
 
-        // 顯示 SweetAlert 窗口
-        Swal.fire({
-            scrollbarPadding: false,
-            title: '成功！',
-            text: '你的申請已成功提交。',
-            icon: 'success',
-            confirmButtonText: '確定'
-        });
-    });
+//         // 顯示 SweetAlert 窗口
+//         Swal.fire({
+//             scrollbarPadding: false,
+//             title: '成功！',
+//             text: '你的申請已成功提交。',
+//             icon: 'success',
+//             confirmButtonText: '確定'
+//         });
+//     });
 
-    // 當輸入框被點擊時
-    inputFields.forEach(function (input) {
-        input.addEventListener('click', function () {
-            input.value = ''; // 清空輸入框的值
-        });
-    });
-});
+//     // 當輸入框被點擊時
+//     // inputFields.forEach(function (input) {
+//     //     input.addEventListener('click', function () {
+//     //         input.value = ''; // 清空輸入框的值
+//     //     });
+//     // });
+// });
 
 
 
