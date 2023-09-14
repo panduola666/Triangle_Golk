8e763ee340f8e0ac63a70361a0caafa31cee324e
edieyt <edie1007110@gmail.com>
2023-09-14 21:37:50 +0800

    聊天室js問題修正

--- assets/js/classroom.js
+++ assets/js/classroom.js
@@ -41,7 +41,6 @@ var controller = (function (botCntr, uiCntr) {
         $chatWraper.show();
     }
 
-<<<<<<< HEAD
  
     function init() {
         $chatCircle = $("#chat-circle");
@@ -77,91 +76,3 @@ var controller = (function (botCntr, uiCntr) {
 
 $(document).ready(controller.init);
     { } 
-=======
-    //generate messages on submit click
-    // function submitMsg(evt) {
-    //     evt.preventDefault();
-
-    //     //1. get input message data
-    //     msg = $chatSubmitBtn.val();
-
-    //     //2.if there is no string button send shoudn't work
-    //     if (msg.trim() == '') {
-    //         return false;
-    //     }
-    //     //3. add message to bot controller
-    //     callbot(msg);
-    //     //4. display message to ui controller
-    //     generate_message(msg, 'self');
-
-    // }
-
-    // function chatSbmBtn(evt) {
-    //     if (evt.keyCode === 13 || evt.which === 13) {
-    //         console.log("btn pushed");
-    //     }
-    // }
-    /* var input = uiCntr.getInput();*/
-    /* $chatSubmitBtn.on("click", hideCircle);*/
-
-
-
-    function init() {
-        $chatCircle = $("#chat-circle");
-        $chatBox = $(".chat-box");
-        $chatBoxClose = $(".chat-box-toggle");
-        $chatBoxWelcome = $(".chat-box-welcome__header");
-        $chatWraper = $("#chat-box__wraper");
-        $chatInput = $("#chat-input__text");
-        $submitBtn = $("#chat-submit");
-
-        //1. call toggle 
-        $chatCircle.on("click", hideCircle);
-        $chatBoxClose.on("click", chatBoxCl);
-        $chatInput.on("click", chatOpenMessage);
-
-        //2. call wait message from CRM-human
-
-        $submitBtn.on("click", chatSbmBtn);
-        $chatInput.on("keypress", chatSbmBtn);
-
-
-        //6. get message from bot controller-back end
-        //7. display bot message to ui controller
-    }
-
-    return {
-        init: init
-    };
-
-})(botController, uiController);
-
-
-// $('.chat-input__form').on('submit', function (e) {
-//     e.preventDefault();
-//   msg = $('.chat-input__text').val();
-  
-//   $('.chat-logs').append('<div id="cm-msg-0" class="chat-msg background-warning push-right bot"><div class="cm-msg-text">' + msg + '</div><span class="msg-avatar"><img class="chat-box-overlay_robot" src="https://www.meetsource.com//userStyles/images/user.png"></span></div>');  
-//   $('.chat-input__text').val('');
-// });
-
-
-$(document).ready(controller.init);
-
-    know = {
-    //   "嗨" : "哈囉",
-    //   "大家好" : "有人在嗎",
-    //   "哈哈" : ":)"
-    // };
-    // function talk() {
-    //   var user = document.getElementById("userBox").value;
-    //   document.getElementById("userBox").value = "";
-    //   document.getElementById("chatLog").innerHTML += user+"<br>";
-    //   if (user in know) {
-    //     document.getElementById("chatLog").innerHTML += know[user]+"<br>";
-    //   } 
-    //   else {
-    //     document.getElementById("chatLog").innerHTML += "我不知道...<br>";
-    //   }
-    } 
->>>>>>> 2c1e01ced007161741a881f16adc67a4c0eee90a
