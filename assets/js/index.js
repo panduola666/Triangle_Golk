cbcd0a79d3fe0e3a92dccbaeb22b3fd7c3ca0a39
panduola666 <panduola666@gmail.com>
2023-09-17 09:40:15 +0800

    [fix] swiper bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,30 @@ const tagList = document.querySelector('.index-tag-list')
 const commentList = document.querySelector('.swiper-wrapper')
 const coursesList = document.querySelector('.new-courses')
 
+// swiper 配置
+document.addEventListener('DOMContentLoaded', function() {
+  const swiper = new Swiper('.other-courses', {
+    // 配置你使用的 swiper class 名稱
+    slidesPerView: 'auto', // 一頁幾張
+    spaceBetween: 16, // 每一頁的間距
+    centeredSlides: false,
+    loop: true, // 循環播放
+    modules: [Navigation, Autoplay, Pagination],
+    autoplay: {
+      // 自動撥放配置
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-next', // 控制上一頁箭頭用哪個 class
+      prevEl: '.swiper-pre',
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      // clickable: true
+    },
+    });
+});
 
 async function init () {
   loading()
@@ -106,25 +130,5 @@ tagList.addEventListener('click', (e) => {
   location.href = `course.html?q=${e.target.textContent}`
 })
 
-// swiper 配置
-const swiper = new Swiper('.other-courses', {
-  // 配置你使用的 swiper class 名稱
-  slidesPerView: 'auto', // 一頁幾張
-  spaceBetween: 16, // 每一頁的間距
-  centeredSlides: false,
-  loop: true, // 循環播放
-  modules: [Navigation, Autoplay, Pagination],
-  autoplay: {
-    // 自動撥放配置
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-next', // 控制上一頁箭頭用哪個 class
-    prevEl: '.swiper-pre',
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  });
+
   
